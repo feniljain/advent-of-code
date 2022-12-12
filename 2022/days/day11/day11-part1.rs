eea65e472f4402b747328e7e6b06c44cab87a76c
feniljain <fkjainco@gmail.com>
2022-12-12 10:24:00 +0530

    feat: day 11 solutions

--- 2022/days/day11/day11-part1.rs
+++ 2022/days/day11/day11-part1.rs
@@ -3,29 +3,29 @@
 use std::{fmt::Debug, fs};
 
 struct Monkey {
-    items_worry_levels: Vec<u128>,
-    operation: Box<dyn Fn(u128) -> u128>,
-    test: Box<dyn Fn(u128) -> usize>,
+    items_worry_levels: Vec<i32>,
+    operation: Box<dyn Fn(i32) -> i32>,
+    test: Box<dyn Fn(i32) -> usize>,
 }
 
 impl Debug for Monkey {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("Monkey")
             .field("starting_items", &self.items_worry_levels)
-            .field("operation", &(self.operation)(2u128))
-            .field("test", &(self.test)(2u128))
+            .field("operation", &(self.operation)(2i32))
+            .field("test", &(self.test)(2i32))
             .finish()
     }
 }
 
-fn make_operation_closure(operation: &str, var: &str) -> Box<dyn Fn(u128) -> u128> {
-    let var_res = var.parse::<u128>();
+fn make_operation_closure(operation: &str, var: &str) -> Box<dyn Fn(i32) -> i32> {
+    let var_res = var.parse::<i32>();
     if let Ok(var) = var_res {
         match operation {
             "+" => Box::new(move |x| var + x),
             "-" => Box::new(move |x| var - x),
             "*" => Box::new(move |x| var * x),
-            "/" => Box::new(move |x| (var as f32 / x as f32).round() as u128),
+            "/" => Box::new(move |x| (var as f32 / x as f32).round() as i32),
             _ => panic!("incorrect operation"),
         }
     } else {
@@ -39,28 +39,19 @@ fn make_operation_closure(operation: &str, var: &str) -> Box<dyn Fn(u128) -> u12
     }
 }
 
-// fn print_monkey_state(monkeys: &Vec<Monkey>) {
-//     // println!("========");
-//     for (i, monkey) in monkeys.iter().enumerate() {
-//         println!("Monkey {}: {:?}", i, monkey.items_worry_levels);
-//     }
-//     // println!("========");
-// }
+fn print_monkey_state(monkeys: &Vec<Monkey>) {
+    println!("========");
+    for (i, monkey) in monkeys.iter().enumerate() {
+        println!("Monkey {}: {:?}", i, monkey.items_worry_levels);
+    }
+    println!("========");
+}
 
-// fn print_round_state(monkeys: &Vec<Monkey>, inspection_count: &Vec<u128>) {
-//     for (i, _monkey) in monkeys.iter().enumerate() {
-//         println!("Monkey {i} inspected items {} times", inspection_count[i]);
-//         // println!("Monkey {}: {:?}", i, monkey.items_worry_levels);
-//     }
-// }
-
-const TOTAL_ROUNDS: u32 = 10000;
+const TOTAL_ROUNDS: u32 = 20;
 
 fn main() {
     let input_str =
-        fs::read_to_string("days/day11/input-day11").expect("should contain input");
-
-    let mut lcm = 1;
+        fs::read_to_string("days/day11/example-input-day11").expect("should contain input");
 
     let mut monkeys = input_str
         .trim()
@@ -72,16 +63,16 @@ fn main() {
             let (_, numbers_str_list) = lines[0]
                 .split_once(":")
                 .expect("invalid input: ':' separation not present");
-            let numbers_list: Vec<u128> = numbers_str_list
+            let numbers_list: Vec<i32> = numbers_str_list
                 .trim()
                 .split(",")
                 .map(|number_str| {
                     number_str
                         .trim()
-                        .parse::<u128>()
+                        .parse::<i32>()
                         .expect("invalid input: expected a number")
                 })
-                .collect::<Vec<u128>>();
+                .collect::<Vec<i32>>();
 
             // operation
             let (_, operation_str) = lines[1]
@@ -101,22 +92,20 @@ fn main() {
                 .expect("invalid input, given string not present");
 
             let divisible_by = divisible_by_str
-                .parse::<u128>()
+                .parse::<i32>()
                 .expect("expected divisible_by integer");
-
-            lcm*=divisible_by;
 
             let true_case = lines[3]
                 .split_once("If true: throw to monkey ")
                 .expect("invalid input, given string not present");
 
-            let true_monkey_idx = true_case.1.parse::<usize>().expect("expected an u128");
+            let true_monkey_idx = true_case.1.parse::<usize>().expect("expected an i32");
 
             let false_case = lines[4]
                 .split_once("If false: throw to monkey ")
                 .expect("invalid input, given string not present");
 
-            let false_monkey_idx = false_case.1.parse::<usize>().expect("expected an u128");
+            let false_monkey_idx = false_case.1.parse::<usize>().expect("expected an i32");
 
             let monkey = Monkey {
                 items_worry_levels: numbers_list,
@@ -136,7 +125,7 @@ fn main() {
 
     let n_monkeys = monkeys.len();
 
-    let mut inspection_count = vec![0u128; n_monkeys];
+    let mut inspection_count = vec![0; n_monkeys];
 
     for _round in 0..TOTAL_ROUNDS {
         for i in 0..n_monkeys {
@@ -147,13 +136,13 @@ fn main() {
                 .items_worry_levels
                 .iter()
                 .for_each(|item_worry_level| {
-                    let worry_level_after_hold = (monkey.operation)(*item_worry_level) % lcm;
+                    let worry_level_after_hold = (monkey.operation)(*item_worry_level);
 
-                    // let worry_level_after_leave = (worry_level_after_hold as f32 / 3.0) as u128;
+                    let worry_level_after_leave = (worry_level_after_hold as f32 / 3.0) as i32;
 
-                    let pass_to_monkey_idx = (monkey.test)(worry_level_after_hold) as usize;
+                    let pass_to_monkey_idx = (monkey.test)(worry_level_after_leave) as usize;
 
-                    items_to_pass.push((pass_to_monkey_idx, worry_level_after_hold % lcm));
+                    items_to_pass.push((pass_to_monkey_idx, worry_level_after_leave));
                     inspection_count[i] += 1;
                 });
 
@@ -165,13 +154,9 @@ fn main() {
                     .push(item_to_pass.1);
             }
         }
-
-        // print_monkey_state(&monkeys);
-        // print_inspection_state(&monkeys);
-
-        // println!("After round: {}", _round+1);
-        // print_round_state(&monkeys, &inspection_count);
     }
+
+    print_monkey_state(&monkeys);
 
     inspection_count.sort_by(|a, b| b.partial_cmp(a).unwrap());
     println!(
